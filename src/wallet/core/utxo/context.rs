252b3f9ef3653f71611a3b116e51ebe9547fbbe9
smartgoo <cvallone28@gmail.com>
2026-02-25 17:14:27 -0500

    override params interpreted as typing.any

--- src/wallet/core/utxo/context.rs
+++ src/wallet/core/utxo/context.rs
@@ -34,7 +34,10 @@ impl PyUtxoContext {
     ///     id: Optional 32-byte hex id (string) or Hash.
     #[new]
     #[pyo3(signature = (processor, id=None))]
-    pub fn ctor(processor: PyUtxoProcessor, id: Option<Bound<'_, PyAny>>) -> PyResult<Self> {
+    pub fn ctor(
+        processor: PyUtxoProcessor,
+        #[gen_stub(override_type(type_repr = "str | Hash"))] id: Option<Bound<'_, PyAny>>,
+    ) -> PyResult<Self> {
         let binding = if let Some(value) = id {
             if let Ok(hash) = value.extract::<PyHash>() {
                 UtxoContextBinding::Id(UtxoContextId::new(hash.into()))
